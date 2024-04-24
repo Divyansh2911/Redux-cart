41772fdc2c518bab7deac26b741db921343615e1
Divyansh2911 <divyansh.sharma292002@gmail.com>
2024-04-25 00:44:44 +0530

    added repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import Navbar from "./Components/Navbar";
+
+export default function App() {
+  // const [state,setState]= useState("")
+  // setState("")
+  // console.log(state)
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!a
+      <Navbar/>
+    </h1>
+  )
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,8 @@
+// import React, { useState } from 'react'
+
+export default function Navbar() {
+    // const [isOpen,setIsOpen] = useState(false);
+  return (
+    <div> <h1 className="text-xxl text-red-800"> Hey</h1></div>
+  )
+}
